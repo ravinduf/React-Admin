3f2cf66aaf769d47e23ad261769b35d4c2152e23
ravinduf <ravinduf@rocketmail.com>
2020-10-31 05:11:44 +0530

    added customizable user component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import UserList from './components/Users'
 
 const dataProvider = jsonServerProvider(`${process.env.REACT_APP_API_URL}`);
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+    <Admin dataProvider={dataProvider} >
+        <Resource name="users" list={UserList} />
+    </Admin>
+    
+);
 
 export default App;
